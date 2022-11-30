2e91f50fa559d861883c4f1dc31456719444e1dc
grassfinn <theharper@live.com>
2022-11-29 22:30:05 -0500

    Intial Commit Logged Key Presses
    Co-authored-by: Sadie <sadiejay@users.noreply.github.com>

---
+++ 01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,17 @@
+
+// todo
+// log the key presss
+// play the sound based of key press
+function logKeys(e){
+    console.log(e)
+    // console log for asdfghj
+    if (e.key === 'a'){
+        console.log('You pressed A')
+        // play sound
+        // change css classes
+    }
+}
+// listening for the keydown
+document.body.addEventListener('keydown', logKeys)
+
+console.log('123')
