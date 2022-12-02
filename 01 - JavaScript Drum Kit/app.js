c307254e34a831019895ef438ea58a937bdce1b5
grassfinn <theharper@live.com>
2022-12-01 21:20:15 -0500

    added functions to add and remove classes based on theaudio duration/playing

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,24 +1,38 @@
 // https://courses.wesbos.com/account/access/6386bd6740307fbbde500e5b
 
+const sounds = document.querySelectorAll('audio')
 
-
-// todo
-// log the key press
-// connect to the audio
-// play the sound based of key press
 function logKeys(e){
-    console.log(e)
-    // console log for asdfghj
-    if (e.key === 'a'){
-        console.log('You pressed A')
-        sounds[0].play();
-
-
-        // play sound
-        // change css classes
+    // connects the audio tag to the corresponding key that has the same key/code data attribute
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`)
+    const key = document.querySelector(`.key[data-key="${e.code}"]`)
+    
+    removeClass = () => {
+      if (audio.ended) {
+        key.classList.remove('playing');
+      }
+      // if (!audio.ended){
+      //     return
+      // }
+      // key.classList.remove('playing')
     }
+    // if the user is pressing a key that does not have an audio tag attached to it we want return the
+    if (!audio) {
+        return
+    };
+    // reset the audio so we can play it as many times as we want
+    audio.currentTime = 0 // current time is the durration of the audio obj
+    audio.play()
+
+    // add class to key is down when / sounding is playing
+    key.classList.add('playing')
+    // remove when key is down / remove the sound finishes
+
+    // removes class when sound clip stops playing
+    setInterval(removeClass,audio.duration)
+
+  
 }
 // listening for the keydown
 document.body.addEventListener('keypress', logKeys)
 
-console.log('123')
