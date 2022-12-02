d2d157f8b4d6569fda2e83f6c7d1888398176869
grassfinn <theharper@live.com>
2022-12-01 20:14:04 -0500

    commenting

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,17 +1,24 @@
+// https://courses.wesbos.com/account/access/6386bd6740307fbbde500e5b
+
+
 
 // todo
-// log the key presss
+// log the key press
+// connect to the audio
 // play the sound based of key press
 function logKeys(e){
     console.log(e)
     // console log for asdfghj
     if (e.key === 'a'){
         console.log('You pressed A')
+        sounds[0].play();
+
+
         // play sound
         // change css classes
     }
 }
 // listening for the keydown
-document.body.addEventListener('keydown', logKeys)
+document.body.addEventListener('keypress', logKeys)
 
 console.log('123')
